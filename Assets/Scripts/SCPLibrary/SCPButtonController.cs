e60135ef776fa5311354f220a72b05fa78488c97
Artem Yurchenko <zumikq78@gmail.com>
2022-01-22 19:20:26 +0100

    SCPButtonController class was implemented

---
+++ Assets/Scripts/SCPLibrary/SCPButtonController.cs
@@ -0,0 +1,24 @@
+using UnityEngine.UIElements;
+
+namespace SCPLibrary
+{
+	public class SCPButtonController
+	{
+		private Button SCPButton { get; set; }
+
+		public void SetVisualElement(VisualElement rootElement)
+		{
+			SCPButton = rootElement.Q<Button>("SCPButton");
+		}
+
+		public void SetCharacterData(uint ID, SCPLibraryManager libraryManager)
+		{
+			SCPButton.text = $"SCP-{ID:D3}";
+
+			SCPButton.clicked += () =>
+			{
+				libraryManager.StartVisualization(ID);
+			};
+		}
+	}
+}
