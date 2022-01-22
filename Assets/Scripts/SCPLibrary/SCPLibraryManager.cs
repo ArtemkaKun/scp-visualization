6548538a7cd43dec5fa0135f63e679b16cbb173e
Artem Yurchenko <zumikq78@gmail.com>
2022-01-22 19:52:39 +0100

    SCPLibraryManager class - StartVisualization method logic was implemented

--- Assets/Scripts/SCPLibrary/SCPLibraryManager.cs
+++ Assets/Scripts/SCPLibrary/SCPLibraryManager.cs
@@ -1,14 +1,24 @@
 using UnityEngine;
+using VisualizationManagement;
+
 namespace SCPLibrary
 {
 	public class SCPLibraryManager : MonoBehaviour
 	{
 		[field: SerializeField]
 		public SCPMetadata[] MetadataCollection { get; private set; }
+		[field: SerializeField]
+		private VisualizationManager VisualizationManager { get; set; }
 		
 		public void StartVisualization (uint ID)
 		{
-			Debug.Log(ID);
+			foreach (SCPMetadata metadata in MetadataCollection)
+			{
+				if (metadata.ID == ID)
+				{
+					VisualizationManager.LoadVisualization(metadata.SceneReference);
+				}
+			}
 		}
 	}
 }
